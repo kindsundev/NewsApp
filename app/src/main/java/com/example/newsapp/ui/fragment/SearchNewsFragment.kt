a6954de19c10afcb337e879c2b213e388f867388
kind sun dev <mr.duongtiendev@gmail.com>
2023-02-15 17:43:23 +0700

    Handling search news response

--- app/src/main/java/com/example/newsapp/ui/fragment/SearchNewsFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/fragment/SearchNewsFragment.kt
@@ -1,20 +1,34 @@
 package com.example.newsapp.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.newsapp.adapter.NewsAdapter
 
 import com.example.newsapp.databinding.FragmentSearchNewsBinding
+import com.example.newsapp.model.NewsResponse
 import com.example.newsapp.ui.NewsActivity
 import com.example.newsapp.ui.NewsViewModel
+import com.example.newsapp.util.Constants
+import com.example.newsapp.util.Resource
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SearchNewsFragment : Fragment() {
 
+    private val TAG = "SearchNewsFragment"
     private var _binding : FragmentSearchNewsBinding? = null
     private val binding get() = _binding
     private lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,6 +43,58 @@ class SearchNewsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as NewsActivity).viewModel
+        setupRecyclerView()
+
+        var job: Job? = null
+        binding!!.etSearch.addTextChangedListener { editable ->
+            job?.cancel()
+            job = MainScope().launch {
+                delay(Constants.SEARCH_NEWS_TIME_DELAY)
+                editable?.let {
+                    if (editable.toString().isNotEmpty()) {
+                        viewModel.searchNews(editable.toString())
+                    }
+                }
+            }
+        }
+
+        setupObservers()
+    }
+
+    private fun setupRecyclerView() {
+        newsAdapter = NewsAdapter()
+        binding!!.rvSearchNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
+
+    private fun setupObservers(){
+        viewModel.searchNews.observe(viewLifecycleOwner, Observer{ response ->
+            when(response) {
+                is Resource.Success<NewsResponse> -> {
+                    hideProgressBar()
+                    response.data?.let { newsResponse ->  
+                        newsAdapter.differ.submitList(newsResponse.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message?.let { message ->
+                        Log.e(TAG, "An error occurred: $message")
+                    }
+                }
+                is Resource.Loading -> { showProgressBar() }
+            }
+        })
+    }
+
+    private fun hideProgressBar() {
+        binding!!.paginationProgressBar.visibility = View.INVISIBLE
+    }
+
+    private fun showProgressBar() {
+        binding!!.paginationProgressBar.visibility = View.VISIBLE
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/newsapp/util/Constants.kt
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -5,5 +5,6 @@ class Constants {
     companion object {
         const val API_KEY = "a27c7bc3045c4e988c5d6a14e02aa007"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
